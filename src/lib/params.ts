54e0b3444fe4cd50b02e4e7909634280c0c0771a
Addis <addis.ugr-4143-15@aau.edu.et>
2025-08-29 12:19:20 +0300

    feat: refactor filtering logic and implement search input component

---
+++ src/lib/params.ts
@@ -0,0 +1,7 @@
+import { parseAsString, createLoader } from "nuqs/server";
+
+export const filterParams = {
+  search: parseAsString.withDefault("").withOptions({ clearOnDefault: true }),
+  category: parseAsString.withDefault("").withOptions({ clearOnDefault: true }),
+};
+export const loadSearchParams = createLoader(filterParams);
