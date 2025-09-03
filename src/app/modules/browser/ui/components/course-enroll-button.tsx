9fc6d303d6020e2d72d07df919ffc889fc5cefdc
Addis <addis.ugr-4143-15@aau.edu.et>
2025-09-03 22:22:28 +0300

    feat(payment): integrate Stripe for course checkout and add webhook handling

--- src/app/modules/browser/ui/components/course-enroll-button.tsx
+++ src/app/modules/browser/ui/components/course-enroll-button.tsx
@@ -2,6 +2,10 @@
 
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
 
 interface Props {
   courseId: string;
@@ -9,8 +13,37 @@ interface Props {
 }
 
 export const CourseEnrollButton = ({ courseId, price }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const trpc = useTRPC();
+  const checkout = useMutation(
+    trpc.payment.createCheckout.mutationOptions({
+      onSuccess: async (data) => {
+        if (data?.url) {
+          window.location.assign(data.url);
+        } else {
+          toast.error("Checkout URL not found.");
+        }
+      },
+      onError: () => {},
+    })
+  );
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      checkout.mutate({ courseId });
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <Button size={"sm"} className="w-full md:w-auto">
+    <Button
+      onClick={onClick}
+      disabled={isLoading}
+      size={"sm"}
+      className="w-full md:w-auto"
+    >
       Enroll for {formatPrice(price)}{" "}
     </Button>
   );
